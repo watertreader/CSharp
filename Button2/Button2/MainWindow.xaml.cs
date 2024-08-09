77719071fc20539bef341c97b3005013c2dd7942
watertreader <watertreader@hotmail.com>
2024-08-09 10:27:35 +0800

    Project to give example on how to add button

---
+++ Button2/Button2/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Button2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var btn1 = new Button { Content = "Send_Fault" };
+        }
+
+
+        void OnClick(object sender, RoutedEventArgs e)
+        {
+            if (FCC_Label.Foreground == Brushes.Yellow)
+            {
+                FCC_Label.Foreground = Brushes.Green;
+            }
+            else if(FCC_Label.Foreground == Brushes.Green)
+            {
+                FCC_Label.Foreground = Brushes.Red;
+            }
+            else
+            {
+                FCC_Label.Foreground = Brushes.Yellow;
+            }
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+ProtocolType.Udp);
+
+            IPAddress serverAddr = IPAddress.Parse("192.168.2.255");
+
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, 11000);
+
+            string text = "Hello";
+            byte[] send_buffer = Encoding.ASCII.GetBytes(text);
+
+            sock.SendTo(send_buffer, endPoint);
+        }
+
+        public void SendMessage(string message)
+        {
+            var data = Encoding.Default.GetBytes(message);
+            using (var udpClient = new UdpClient(AddressFamily.InterNetwork))
+            {
+                var address = IPAddress.Parse("224.100.0.1");
+                var ipEndPoint = new IPEndPoint(address, 8088);
+                udpClient.JoinMulticastGroup(address);
+                udpClient.Send(data, data.Length, ipEndPoint);
+                udpClient.Close();
+            }
+        }
+    }
+}
